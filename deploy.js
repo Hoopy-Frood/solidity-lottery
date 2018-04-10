4c051df50a9c9064928b48aa1cb07b630fa668fb
Andrew Chan <andrew.cw.chan@gmail.com>
2018-04-10 11:02:02 +0800

    Second commit

--- deploy.js
+++ deploy.js
@@ -2,9 +2,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require ('web3');
 const { interface,bytecode } = require ('./compile');
 
-const provider = new HDWalletProvider (
 
-);
 
 const web3 = new Web3(provider);
 
